785134ebdb287100854a3f1565148b21fe4c3883
sks <2979368807@qq.com>
2026-05-14 09:37:44 +0800

    second-commit

--- src/main/java/com/sks/JenkinsApplication.java
+++ src/main/java/com/sks/JenkinsApplication.java
@@ -2,12 +2,19 @@ package com.sks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Jenkins!";
+    }
 }
